8d861cc523a257d955fc2474851d40fd10e40640
Jordan-Demske <jmdemske@outlook.com>
2021-10-12 14:44:05 -0400

    Smooth player movement accounting for delta

--- Project/Player/Player.gd
+++ Project/Player/Player.gd
@@ -1,16 +1,23 @@
 extends KinematicBody2D
 
-var velocity = Vector2.ZERO
+const _ACCELERATION = 10
+const _FRICTION = 400
+const _MAX_SPEED = 50
+
+var _velocity = Vector2.ZERO
 
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		_velocity += input_vector * _ACCELERATION * delta
+		_velocity = _velocity.clamped(_MAX_SPEED * delta)
 	else:
-		velocity = Vector2.ZERO
+		_velocity = _velocity.move_toward(Vector2.ZERO, _FRICTION * delta)
 	
-	move_and_collide(velocity)
+	print(_velocity)
+	return move_and_collide(_velocity)
