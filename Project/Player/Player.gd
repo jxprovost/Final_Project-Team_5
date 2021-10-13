7d1a6bffb0484e35da070c4ed3affc31a4bc2691
Jordan-Demske <jmdemske@outlook.com>
2021-10-13 00:03:47 -0400

    Add Player animations

--- Project/Player/Player.gd
+++ Project/Player/Player.gd
@@ -6,6 +6,8 @@ const _MAX_SPEED = 100
 
 var _velocity = Vector2.ZERO
 
+onready var animatedSprite = $AnimatedSprite
+
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,8 +16,10 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animatedSprite.play("Run")
 		_velocity = _velocity.move_toward(input_vector * _MAX_SPEED, _ACCELERATION * delta)
 	else:
+		animatedSprite.play("Idle")
 		_velocity = _velocity.move_toward(Vector2.ZERO, _FRICTION * delta)
 	
 	print(_velocity)
