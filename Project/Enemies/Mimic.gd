e741d3ad4c4eac8588fa728e7bd8188d66b2a844
Jaren Provost <70344370+jxprovost@users.noreply.github.com>
2021-11-18 20:17:34 -0500

    Add to mimic

--- Project/Enemies/Mimic.gd
+++ Project/Enemies/Mimic.gd
@@ -5,6 +5,13 @@ enum State {
 	ACTIVE,
 	ATTACK
 	}
+	
+	
 
 func _ready():
+	$AnimatedSprite.animation = "chest"
 	pass # Replace with function body.
+
+func _physics_process(_delta):
+	pass
+	# mimic activates once it has been interacted with 
