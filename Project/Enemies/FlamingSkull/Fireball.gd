3f63d1b4569e4d1127485b2ba807341c4c9265e6
Jaren Provost <70344370+jxprovost@users.noreply.github.com>
2021-12-07 23:54:36 -0500

    Work on flaming skull and attempt to make adjustments to mimic
    Mimic can lunge

---
+++ Project/Enemies/FlamingSkull/Fireball.gd
@@ -0,0 +1,58 @@
+extends KinematicBody2D
+
+enum State {
+	INACTIVE,
+	FORMING,
+	PREPARING,
+	LAUNCH
+	}
+	
+const EnemyDeathEffect := preload("res://Effects/EnemyDeathEffect.tscn")
+
+var _state = State.INACTIVE
+var _fireball := "active"
+var _form := false
+var _prep := false
+var _launch := false
+var _direction := 0
+
+var _velocity := Vector2.ZERO
+
+func _ready():
+	$AnimatedSprite.play("default")
+	$Timer.start()
+
+
+func _process(delta):
+
+	if $Timer.time_left >= 6:
+		_state = State.FORMING
+	elif $Timer.time_left >= 3:
+		_state = State.PREPARING
+	elif $Timer.time_left < 3:
+		_state = State.LAUNCH
+		
+	match _state:
+		State.FORMING: 
+			if _form == false:
+				$AnimationPlayer.play("FormingProjectile")
+				_form = true
+
+		State.PREPARING:
+			if _prep == false:
+				$AnimationPlayer.play("PrepareLaunch")
+				_prep = true
+		
+		State.LAUNCH:
+			var player = $PlayerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				_velocity = _velocity.move_toward(direction * 1000, 500 * delta)
+			else:
+				_state = State.INACTIVE
+				
+			_velocity = move_and_slide(_velocity)
+			
+			
+func _on_Timer_timeout():
+	queue_free()
