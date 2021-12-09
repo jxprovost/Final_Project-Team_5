4c71a3ce24d3ccdbfa49fbab042c6b1411526cdb
Jesse <jdcoker@bsu.edu>
2021-12-08 22:43:59 -0500

    Fix barrel and hall

--- Project/World/DF1/Interactive/Barrel/Barrel.gd
+++ Project/World/DF1/Interactive/Barrel/Barrel.gd
@@ -11,12 +11,6 @@ func destroy():
 	if not destroyed:
 		destroyed = true
 		$Sprite.frame = 1
-		call_deferred("changeCollisionShape")
 		$DestroyedParticles.emitting = true
 		$DestroyedTopParticle.emitting = true
 		$BreakSoundPlayer.play()
-
-
-func changeCollisionShape():
-	$DefaultCollisionShape2D.disabled = true
-	$DestroyedCollisionShape2D.disabled = false
--- Project/World/DF1/Interactive/InstantTeleporter/InstantTeleporter.gd
+++ Project/World/DF1/Interactive/InstantTeleporter/InstantTeleporter.gd
@@ -7,12 +7,9 @@ var camera : Camera2D = null
 func _ready():
 	world = get_tree().get_root().get_node("World")
 	camera = world.get_node("Camera2D")
-	print(camera.name)
 
 
 func _on_InstantTeleporter_body_entered(body):
 	if body.name == "Player":
-		camera.smoothing_enabled = false
 		body.position = Vector2($TeleportPosition.global_position.x, body.position.y)
 		camera.position = body.position
-		camera.smoothing_enabled = true
